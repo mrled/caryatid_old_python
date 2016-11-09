cfd1c1727f782b07a4dc3a09ea9f2407abf99110
Micah R Ledbetter <micah@doublelinepartners.com>
2016-11-09 10:33:58 -0600

    __init__.py should be empty

--- __init__.py
+++ __init__.py
@@ -1,6 +1,1 @@
 #!/usr/bin/env python3
-
-# CARYATID:
-# In architecture, an Atlas [https://atlas.hashicorp.com/] is a man taking the place of a column, and a Caryatid is a woman taking the place of a column
-
-import caryatid
